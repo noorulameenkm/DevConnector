c08dbce6c723db2b0b7a86eb4495ccde7d5a7f14
lakdivian <noorul.km@people10.com>
2019-10-12 18:16:46 +0530

    Added redux and redux store has been setup for the application

--- client/src/App.js
+++ client/src/App.js
@@ -6,17 +6,24 @@ import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 
+// import provider and store and wrap the entire components with provider
+import { Provider } from 'react-redux';
+import store from './store';
+
+
 const App = () => 
+  <Provider store={ store }>
     <Router>
       <Fragment>
         <Navbar />
-        <Route exact path='/' component={Landing} />
+        <Route exact path='/' component={ Landing } />
         <Switch>
           <Route path='/register' component={ Register } />
           <Route path='/login' component={ Login } />
         </Switch>
       </Fragment>
     </Router>
+  </Provider>
 
 
 export default App;
