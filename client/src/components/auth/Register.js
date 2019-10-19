a5b783eaa9af2c0a03c2eaeca5114cb8b9cb379a
lakdivian <noorul.km@people10.com>
2019-10-19 21:51:06 +0530

    Added types, actions and alert component

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react'
+import { connect } from 'react-redux';
+import { setAlert } from '../../actions/alert';
 import { Link } from 'react-router-dom';
+import Alert from '../layout/Alert';
+import PropTypes from 'prop-types';
 
-const Register = () => {
+const Register = ({ setAlert }) => {
 
     const [formData, setFormData] = useState({
         name: '',
@@ -19,6 +23,7 @@ const Register = () => {
         e.preventDefault();
         if(password !== password2){
             console.log("Passwords do not match");
+            setAlert('Passwords do not match', 'danger');
         } else {
             console.log(formData);
         }
@@ -26,6 +31,7 @@ const Register = () => {
 
     return (
         <section className="container">
+            <Alert />
             <h1 className="large text-primary">Sign Up</h1>
             <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
             <form className="form" onSubmit={ onSubmit }>
@@ -84,4 +90,8 @@ const Register = () => {
     );
 }
 
-export default Register
+Register.propTypes = {
+    setAlert: PropTypes.func.isRequired,
+}
+
+export default connect(null,{ setAlert })(Register)
